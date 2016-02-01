8a942e90850fe40260e92422ed5e9a1f122440de
jason.zhang <549944323@qq.com>
2016-02-01 17:04:50 +0800

    demo

--- business-log/src/main/java/com/saferich/business/log/LogRemoteBusiness.java
+++ business-log/src/main/java/com/saferich/business/log/LogRemoteBusiness.java
@@ -1,15 +1,25 @@
 package com.saferich.business.log;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saferich.api.log.ILogRemoteBusiness;
+import com.saferich.dao.log.user.mapper.UserMapper;
+import com.saferich.dao.log.user.model.User;
 
 @Service
 public class LogRemoteBusiness implements ILogRemoteBusiness {
+    @Autowired
+    private UserMapper userMapper;
 
-	@Override
-	public void printOut() {
-		System.out.println("app run!--------------------------------");
-	}
+    @Override
+    public void printOut() {
+        System.out.println("app run!--------------------------------");
+    }
+
+    @Override
+    public void addUser(User user) {
+        userMapper.insertSelective(user);
+    }
 
 }
--- business-log/src/test/java/com/saferich/business/log/LogRemoteServiceTest.java
+++ business-log/src/test/java/com/saferich/business/log/LogRemoteServiceTest.java
@@ -5,20 +5,25 @@ import org.junit.Test;
 
 import com.saferich.api.log.ILogRemoteBusiness;
 import com.saferich.core.factory.RemoteServiceFactory;
+import com.saferich.dao.log.user.model.User;
 
 public class LogRemoteServiceTest {
-	
-	private ILogRemoteBusiness logRemoteService;
-	
-	@Before
+
+    private ILogRemoteBusiness logRemoteService;
+
+    @Before
     public void setUp() throws Exception {
         System.setProperty("business.log.url", "http://127.0.0.1:8080/business-log/remote");
+
         logRemoteService = RemoteServiceFactory.getInstance().service(ILogRemoteBusiness.class);
     }
 
     @Test
     public void testActivityBars() {
-    	logRemoteService.printOut();
-        System.out.println("okok");
+        User user = new User();
+        user.setName("lisi");
+        user.setAge(11);
+        logRemoteService.addUser(user);
+        System.out.println("ok!!!");
     }
 }
