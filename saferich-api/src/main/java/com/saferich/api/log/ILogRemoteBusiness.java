8a4534217aef872a1627f029b79e1cf5222f9f40
jason.zhang <549944323@qq.com>
2016-02-01 17:05:27 +0800

    demo

--- saferich-api/src/main/java/com/saferich/api/log/ILogRemoteBusiness.java
+++ saferich-api/src/main/java/com/saferich/api/log/ILogRemoteBusiness.java
@@ -3,11 +3,14 @@ package com.saferich.api.log;
 import com.saferich.core.Context;
 import com.saferich.core.Hessian;
 import com.saferich.core.RemoteService;
+import com.saferich.dao.log.user.model.User;
 
 @Hessian(context = Context.LOG, uri = "/logBusiness")
-public interface ILogRemoteBusiness  extends RemoteService {
-	/**
-	 * 测试
-	 */
-	void printOut();
+public interface ILogRemoteBusiness extends RemoteService {
+    /**
+     * 测试
+     */
+    void printOut();
+
+    void addUser(User user);
 }
